97de4c58c27e964f794ad132ce2bbfb1e034302e
Mannie <carlos.alvarezberrio@gmail.com>
2023-05-14 20:39:46 -0400

    added start script and process.env.PORT

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Starting point of the server
 const server = require("./api/server");
+const PORT = process.env.PORT || 5001;
 
-server.listen(5001, () => console.log("Server listening on port 5001"));
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
